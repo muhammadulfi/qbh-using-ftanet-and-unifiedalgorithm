396c3568eabc89f34e01e72034b94165ad01ad1c
Muhammad Ulfi <38999012+muhulfi@users.noreply.github.com>
2022-10-31 16:42:46 +0700

    Delete source-code directory

--- source-code/unified_algorithm/random/test_generate_precomputed.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-f = open('E:\Kuliah\S2\Thesis\source code thesis\database\precomputed_feature\ioacas_n3c_mnf.pkl',"rb")
-feature = pickle.load(f)
-
-for key, value in feature.items():
-    print(key, value)
